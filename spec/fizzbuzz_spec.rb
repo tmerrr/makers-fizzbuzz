fe5e481d3a7162045da9a1d67335487def760c0e
Tom Moir <trmoir9@gmail.com>
2017-10-18 16:34:53 +0100

    div? Passes unit tests when Integer is 0

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -12,13 +12,23 @@ describe ".div?" do
 		end
 	end
 
-	context "when the Intger is not perfectly divisible by the value" do
+	context "when the Integer is not perfectly divisible by the value" do
 		it 'returns false' do
 			expect(20.div?(3)).to eq false
 			expect(20.div?(6)).to eq false
 			expect(20.div?(12)).to eq false
 			expect(100.div?(13)).to eq false
 			expect(100.div?(9)).to eq false
+		end
+	end
+
+	context "When 0 is the Integer" do
+		it 'returns true' do
+			expect(0.div?(2)).to eq true
+			expect(0.div?(3)).to eq true
+			expect(0.div?(4)).to eq true
+			expect(0.div?(10)).to eq true
+			expect(0.div?(29)).to eq true
 		end
 	end
 end

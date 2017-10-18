8579a6ebdfdba176d313e785e5ebcb1569974b1d
Tom Moir <trmoir9@gmail.com>
2017-10-18 16:31:07 +0100

    div? method passed unit test for returning false

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -11,4 +11,14 @@ describe ".div?" do
 			expect(20.div?(5)).to eq true
 		end
 	end
+
+	context "when the Intger is not perfectly divisible by the value" do
+		it 'returns false' do
+			expect(20.div?(3)).to eq false
+			expect(20.div?(6)).to eq false
+			expect(20.div?(12)).to eq false
+			expect(100.div?(13)).to eq false
+			expect(100.div?(9)).to eq false
+		end
+	end
 end
